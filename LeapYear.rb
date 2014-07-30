ac16de514c671197c22b72bc1e69f0d780b878ff
Patrick J. Will <patrickwill2121@gmail.com>
2014-07-30 18:17:06 -0400

    add purpose, notes, and initial comments

---
+++ LeapYear.rb
@@ -0,0 +1,11 @@
+# This program asks for a starting and ending year, and outputs every leap year between them
+
+############## NOTES ################
+#all leap years are divisible by four
+#years divisible by 100 are not leap years, unless divisible by 400
+#####################################
+
+
+# ask for a beginning year
+
+# ask for an ending year
