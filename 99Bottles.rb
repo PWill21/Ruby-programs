5988dcc799625aa24cbc5bc26e904a4741d98c4b
Patrick J. Will <patrickwill2121@gmail.com>
2014-07-30 17:52:49 -0400

    add code in loop that removes redundancy of last line song

--- 99Bottles.rb
+++ 99Bottles.rb
@@ -3,10 +3,11 @@ first_line = "bottles of beer on the wall,"
 second_line = "bottles of beer,"
 third_line = "take one down, pass it around,"
 while number_of_bottles > 0
+	repeat_first_line = (number_of_bottles.to_s + " " + first_line) # removes redundancy of last line
 	puts(number_of_bottles.to_s + " " + first_line)
 	puts(number_of_bottles.to_s + " " + second_line)
 	puts(third_line)
-	puts(number_of_bottles.to_s + " " + first_line)
+	puts(repeat_first_line)
 	puts "\n"
 	number_of_bottles = number_of_bottles - 1
 end
