ddab17d42d933abf1d26014a71f5985ccc4d67e1
Patrick J. Will <patrickwill2121@gmail.com>
2014-08-01 02:24:20 -0400

    add variable to track total ounces; add loop to get ounces for each ingredient

--- alcoholContentCalc.rb
+++ alcoholContentCalc.rb
@@ -1,3 +1,12 @@
 #this program calculates the alcohol content of any cocktail, 
 #so long as the user knows the ABV and ounces of each ingredient
 
+puts "How many ingredients in the cocktail?"
+number_of_mixers = gets.chomp.to_i
+total_ounces = 0
+
+number_of_mixers.times do
+	puts "How many ounces of this ingredient?"
+	ounces = gets.chomp.to_i
+	total_ounces = total_ounces + ounces
+end
