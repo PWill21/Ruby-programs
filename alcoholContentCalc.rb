1896022eca71e50cb79f990b4c232c3e7ecb9e80
Patrick J. Will <patrickwill2121@gmail.com>
2014-08-01 02:50:54 -0400

    add variables to calculate and total alcohol volume in ounces

--- alcoholContentCalc.rb
+++ alcoholContentCalc.rb
@@ -4,6 +4,7 @@
 puts "How many ingredients in the cocktail?"
 number_of_mixers = gets.chomp.to_i
 total_ounces = 0
+total_alcohol_vol = 0
 
 number_of_mixers.times do
 	puts "How many ounces of this ingredient?"
@@ -13,4 +14,10 @@ number_of_mixers.times do
 	puts "What is the proof or ABV, in %, of this ingredient?"
 	abv = gets.chomp.to_f
 	perct_alcohol = (abv/100).to_f
+
+	alcohol_vol = ounces * perct_alcohol
+	total_alcohol_vol = total_alcohol_vol + alcohol_vol
 end
+
+puts total_alcohol_vol
+
