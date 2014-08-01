895b9f400ec591f6b534473e09174bd2af0ede5a
Patrick J. Will <patrickwill2121@gmail.com>
2014-08-01 02:39:48 -0400

    add input for ABV; change variables to float

--- alcoholContentCalc.rb
+++ alcoholContentCalc.rb
@@ -7,6 +7,10 @@ total_ounces = 0
 
 number_of_mixers.times do
 	puts "How many ounces of this ingredient?"
-	ounces = gets.chomp.to_i
+	ounces = gets.chomp.to_f
 	total_ounces = total_ounces + ounces
+
+	puts "What is the proof or ABV, in %, of this ingredient?"
+	abv = gets.chomp.to_f
+	perct_alcohol = (abv/100).to_f
 end
