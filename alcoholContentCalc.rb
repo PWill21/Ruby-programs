2f00f1cdcd1e8ea813a85d546f37665279205593
Patrick J. Will <patrickwill2121@gmail.com>
2014-08-03 01:06:33 -0400

    add code that compares cocktail to a standard drink

--- alcoholContentCalc.rb
+++ alcoholContentCalc.rb
@@ -1,19 +1,21 @@
 #this program calculates the alcohol content of any cocktail, 
 #so long as the user knows the ABV and ounces of each ingredient
 
+stand_alc_vol = 0.6 # ounces of alcohol in a standard drink
+
 puts "How many ingredients in the cocktail?"
 number_of_mixers = gets.chomp.to_i
-total_ounces = 0
-total_alcohol_vol = 0
+total_ounces = 0 # total ounces of all ingredients
+total_alcohol_vol = 0 # total volume of alcohol in ounces
 
 number_of_mixers.times do
-	puts "How many ounces of this ingredient?"
+	puts "How many ounces of next ingredient?"
 	ounces = gets.chomp.to_f
 	total_ounces = total_ounces + ounces
 
 	puts "What is the proof or ABV, in %, of this ingredient?"
 	abv = gets.chomp.to_f
-	perct_alcohol = (abv/100).to_f
+	perct_alcohol = (abv/100).to_f   # converts percentage to decimal
 	puts
 
 	alcohol_vol = ounces * perct_alcohol
@@ -21,4 +23,6 @@ number_of_mixers.times do
 end
 
 alcohol_content = (total_alcohol_vol / total_ounces) * 100
+standard_drink = total_alcohol_vol / stand_alc_vol 
+puts ("This is equivalent to " + standard_drink.to_s + " standard drinks")
 puts (alcohol_content.to_s + "% ABV")
